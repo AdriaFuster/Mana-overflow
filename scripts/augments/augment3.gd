268a1d29beba65389cb5af2ab73a9d29574e3279
AdriaFuster <adriafu@gmail.com>
2025-10-31 00:52:46 +0100

    inventory scroll bar + windows size fixed

---
+++ scripts/augments/augment3.gd
@@ -0,0 +1,18 @@
+extends Augment
+class_name Augment3
+
+@export var efect_increment: float = 5
+
+
+func _ready() -> void:
+	super._ready()
+	upgrade_efect()
+	
+	
+func upgrade_efect() -> void:
+	Stats.add_percent_modifier(augment_res.name,efect_increment, augment_res.tick_duration, 
+								Stats.MODIFIER_TYPE.MPS, augment_res.permanent)
+
+
+	
+	
